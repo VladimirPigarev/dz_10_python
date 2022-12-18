56872a414d66d670a1cd48d43eb04fd5c7c18871
VladimirPigarev <109297010+VladimirPigarev@users.noreply.github.com>
2022-12-18 21:48:53 +0300

    подправил код

--- spy.py
+++ spy.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
-async def log(update: Update, context: ContextTypes):
+async def loger(update: Update, context: ContextTypes):
    file = open('db.csv', 'a+', encoding="utf-8")
    file.write(f'{update.effective_user.first_name}, {update.effective_user.id}, {update.message.text}\n')
    file.close()
